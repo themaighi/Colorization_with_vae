da2c25c645353c9a91391e785ca71a7ace11a60e
Michele Cavazza <michele.cavazza@nielsen.com>
2020-07-07 19:18:39 +0200

    finished processing by chuks

--- scripts/m_knn.py
+++ scripts/m_knn.py
@@ -4,7 +4,14 @@ import cv2
 from keras.models import load_model
 import tensorflow as tf
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+import pickle
 
+
+def split_test(folder_path, proportion):
+    list_img = os.listdir(folder_path)
+    train_img, test_img = train_test_split(list_img, test_size=proportion)
+    return train_img, test_img
 
 def read_img(filename, img_size):
     '''Reading images and reshape them depending on the NN needed shape'''
@@ -16,15 +23,17 @@ def read_img(filename, img_size):
     return np.reshape(labimg[:, :, 0], (img_size, img_size, 1)), labimg[:, :, 1:]
 
 
-def import_images(folder_path, proportion):
+def import_images(list_img, folder_path):
     '''Steps needed to import the images
     - Batch = is the list of images Black and white (first layer)
     - Labels = Are the color of the image
     - Filelist = list of files'''
-    list_img = os.listdir(folder_path)
+
+
     n_img = len(list_img)
-    selection_mask = np.random.binomial(1,proportion,size=n_img)
-    img_to_import = [list_img[i] for i in range(n_img) if selection_mask[i] == 1]
+    # selection_mask = np.random.binomial(1,proportion,size=n_img)
+    # img_to_import = [list_img[i] for i in range(n_img) if selection_mask[i] == 1]
+    img_to_import = list_img
     batch = []
     labels = []
     filelist = []
@@ -69,14 +78,13 @@ def transform_image(batch_images, model_classification):
     # transformed_images = model_classification.predict(np.tile(np.asarray(batch_images) / 255, [1, 1, 1, 3]))
     return transformed_images
 
-def knn_fit(tranform_images, classification_labels, *args, **kwargs):
+def knn_fit(dt_dict, *args, **kwargs):
     '''Fit a Knn model'''
-    neigh = KNeighborsClassifier(n_neighbors=3)
-    tranform_images = np.asarray(tranform_images)
+    neigh = KNeighborsClassifier(*args, **kwargs)
+    tranform_images = np.asarray(dt_dict['X'])
     tranform_images = tranform_images.reshape((tranform_images.shape[0],tranform_images.shape[2]))
-    neigh.fit(tranform_images, classification_labels['category_labels'].values)
+    neigh.fit(tranform_images, dt_dict['y'])
 
-    neigh.predict([tranform_images[0],tranform_images[1]])
     return neigh
 def knn_predict(img, knn_model):
     '''Make predictions'''
@@ -84,18 +92,33 @@ def knn_predict(img, knn_model):
     return knn_model.predict(img)
 
 
-def run():
-    batch, labels, filelist = import_images('data/val_256', 0.001)
-    classification_labels = label_images(filelist)
+def process_data():
+    train_img, test_img = split_test('data/val_256', 0.2)
     model_classification = load_model('models/my_model_colorization.h5')
-    tranform_images = transform_image(batch, model_classification)
-    knn_fit(tranform_images, classification_labels)
+    chunk_size = 100
+    transformed_image_list = list()
+    classification_labels_list = list()
+    # stop_index = len(train_img)
+    stop_index = 200
+    for i in range(0,stop_index , chunk_size):
+        chunk = train_img[i:i + chunk_size]
+        batch, labels, filelist = import_images(chunk, 'data/val_256',1)
+        classification_labels = label_images(filelist)
+        tranform_images = transform_image(batch, model_classification)
+        classification_labels_list.extend(classification_labels.category_labels.values)
+        transformed_image_list.extend(tranform_images)
+    output_dict = {'X': transformed_image_list, 'y': classification_labels_list, 'train_img':train_img, 'test_img':test_img}
+    with open('data/d_image_processed_knn.pkl', 'wb') as f:
+        pickle.dump(output_dict, f)
 
-    return 5
-
+def run_knn():
+    with open('data/d_image_processed_knn.pkl', 'rb') as f:
+        dict_var = pickle.load(f)
+    knn_fit(dict_var, n_neighbors=3)
 
 
 if __name__ == '__main__':
     import os
     os.chdir('..')
-    run()
+    # process_data()
+    run_knn()
